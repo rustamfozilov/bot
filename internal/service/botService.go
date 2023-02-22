fa3a319413ebe2c56fab1b8031abe59760e5976a
ssharifzoda <sharifzoda.sino@gmail.com>
2023-02-22 15:12:20 +0500

    jh

--- internal/service/botService.go
+++ internal/service/botService.go
@@ -1,7 +1,6 @@
 package service
 
 import (
-	"errors"
 	"github.com/ssharifzoda/bot/internal/database"
 )
 
@@ -14,13 +13,13 @@ func NewBotService(db database.Bot) *BotService {
 }
 
 func (b *BotService) RegisterUser(userId int64) (string, error) {
-	check, err := b.db.CheckUser(userId)
-	if err != nil {
-		return "", err
-	}
-	if check == true {
-		return "", errors.New("эти данные уже есть в системе")
-	}
+	//check, err := b.db.CheckUser(userId)
+	//if err != nil {
+	//	return "", err
+	//}
+	//if check == true {
+	//	return "", errors.New("эти данные уже есть в системе")
+	//}
 	return b.db.RegisterUser(userId)
 }
 func (b *BotService) RegisterUsernames(userid int64, userName string) (string, error) {
